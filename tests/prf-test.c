84b3c14ab01de1c1c0c4f23065e8d1fd1fffd208
Olaf Bergmann <bergmann@tzi.org>
2011-04-29 01:39:21 +0200

    changed PRF API; new test values

--- tests/prf-test.c
+++ tests/prf-test.c
@@ -10,36 +10,6 @@
 #include "numeric.h"
 #include "hmac.h"
 
-#define DTLS_MASTER_SECRET_LENGTH 48
-
-/** 
- * Generates pre_master_secret from given PSK and fills the result
- * according to the "plain PSK" case in section 2 of RFC 4279.
- * Diffie-Hellman and RSA key exchange are currently not supported.
- *
- * \param key    The shared key.
- * \param keylen Length of \p key in bytes.
- * \param result The derived pre master secret.
- * \return The actual length of \p result.
- */
-static inline size_t
-pre_master_secret(unsigned char *key, size_t keylen,
-		  unsigned char *result) {
-  unsigned char *p = result;
-
-  dtls_int_to_uint16(p, keylen);
-  p += sizeof(uint16);
-
-  memset(p, 0, keylen);
-  p += keylen;
-
-  memcpy(p, result, sizeof(uint16));
-  p += sizeof(uint16);
-  
-  memcpy(p, key, keylen);
-
-  return (sizeof(uint16) + keylen) << 1;
-}
 
 void dump(unsigned char *buf, size_t len) {
   while (len--) 
@@ -55,11 +25,11 @@ int main(int argc, char **argv) {
   int i;
 
   dtls_security_parameters_t sec = { 
-    { 0x4d, 0xb6, 0xfc, 0x22, 0xac, 0x77, 0x38, 0x82, 
-      0xaa, 0x67, 0xf2, 0x2d, 0xdd, 0x22, 0x31, 0xfd, 
-      0x4b, 0xa7, 0x61, 0xf3, 0x6d, 0x96, 0xb5, 0xbe, 
-      0xc0, 0xb6, 0x49, 0x14, 0x6a, 0xc2, 0x49, 0x2f },
-    { 0x4d, 0xb6, 0xfc, 0x22, 0xab, 0x00, 0x00, 0x00, 
+    { 0x4d, 0xb9, 0xca, 0x2f, 0x69, 0xe4, 0x01, 0x00, 
+      0xd4, 0xb9, 0xb2, 0xb4, 0xfd, 0x9c, 0x0c, 0xd8, 
+      0xe4, 0x5f, 0x90, 0x38, 0x18, 0x5c, 0xa5, 0x06, 
+      0xd6, 0xad, 0x23, 0x2e, 0xaf, 0x19, 0xef, 0x07 },
+    { 0x4d, 0xb9, 0xca, 0x2f, 0xab, 0x00, 0x00, 0x00, 
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x13 },
@@ -67,33 +37,18 @@ int main(int argc, char **argv) {
     0				/* compression */
   };
 
-  str master_seeds[] = {
-    { 13, (unsigned char *)"master secret" },
-    /* client random: */
-    { 32, sec.client_random },
-    /* server random: */
-    { 32, sec.server_random }
-  };
-
-  str key_block_seeds[] = {
-    { 13, (unsigned char *)"key expansion" },
-    /* server random: */
-    { 32, sec.server_random },
-    /* client random: */
-    { 32, sec.client_random }
-  };
-
   unsigned char packet[] = {
-    0x16, 0xfe, 0xff, 0x00, 0x01, 0x00, 
-    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x40, 0x35, 
-    0xdf, 0x52, 0xc3, 0x75, 0x82, 0x26, 0x21, 0x76, 
-    0x55, 0xb6, 0xc3, 0x4c, 0x73, 0x35, 0x36, 0x45, 
-    0xa5, 0x18, 0x2e, 0xf6, 0xee, 0xff, 0xf5, 0xf0, 
-    0xd1, 0xee, 0x90, 0x08, 0x20, 0x3f, 0x03, 0x72, 
-    0x52, 0x34, 0x94, 0x27, 0x53, 0xb3, 0xc2, 0x27, 
-    0xbc, 0x7e, 0x97, 0x1f, 0x56, 0x77, 0xcd, 0x0f, 
-    0xa0, 0x01, 0xc7, 0xa3, 0x1e, 0x40, 0x5e, 0x75, 
-    0x08, 0xb7, 0xf9, 0x94, 0x32, 0x4a, 0x08 };
+    0x16, 0xfe, 0xff, 0x00, 0x01, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x38, 0xe3, 
+    0xac, 0x89, 0x15, 0x7e, 0x94, 0x1e, 0x5f, 0x71, 
+    0x10, 0x84, 0x19, 0xdc, 0xfc, 0xa2, 0x8c, 0x06, 
+    0xae, 0x8c, 0x7f, 0xca, 0x47, 0xdb, 0xb3, 0x79, 
+    0xa5, 0x00, 0x92, 0x62, 0x88, 0x13, 0x9b, 0x41, 
+    0xec, 0xf9, 0xec, 0xac, 0x6c, 0xa1, 0x6c, 0x08, 
+    0x25, 0x87, 0x38, 0xc9, 0x02, 0x7c, 0x3e, 0xab, 
+    0x57, 0x6d, 0xa0, 0x77, 0x2c, 0x59, 0xa7, 0xac, 
+    0x39, 0x1c, 0xcf, 0x31, 0x5d, 0xe1
+  };
   unsigned char cleartext[1000];
 
   size_t cleartext_length;
@@ -103,13 +58,15 @@ int main(int argc, char **argv) {
    **********************************************************************/
 
   /* create pre_master_secret */
-  plen = pre_master_secret(key, sizeof(key) - 1, pre_master);
+  plen = dtls_pre_master_secret(key, sizeof(key) - 1, pre_master);
 
   /* create master_secret from pre_master_secret
    * master_secret = PRF(pre_master_secret, 
                          "master secret" + client_random + server_random) */
   len = dtls_prf(pre_master, plen,
-		 master_seeds, sizeof(master_seeds) / sizeof(str),
+		 (unsigned char *)"master secret", 13,
+		 sec.client_random, 32,
+		 sec.server_random, 32,
 		 master_secret, DTLS_MASTER_SECRET_LENGTH);
   
   printf("master_secret:\n");
@@ -121,7 +78,9 @@ int main(int argc, char **argv) {
    * key_block = PRF(master_secret,
                      "key expansion" + server_random + client_random) */
   key_block_len = dtls_prf(master_secret, len,
-			   key_block_seeds, sizeof(key_block_seeds) / sizeof(str),
+			   (unsigned char *)"key expansion", 13,
+			   sec.server_random, 32,
+			   sec.client_random, 32,
 			   sec.key_block, 
 			   dtls_kb_size(&sec));
 
@@ -158,7 +117,7 @@ int main(int argc, char **argv) {
   /**********************************************************************
    * decrypt 
    **********************************************************************/
-  if (!dtls_decrypt(&sec,
+  if (!dtls_cbc_decrypt(&sec,
 		    packet, sizeof(packet),
 		    cleartext, &cleartext_length)) {
     printf("decryption failed!\n");
@@ -199,4 +158,21 @@ ab 15 b1 7a 64 a5 89 2b 36 75 96 42 86 c1 1c 1e   /* random */
 
 /* ciphertext: */
 a9 1c 46 b2 9a 11 54 19 2a 30 d3 ad e1 69 d8 7a d3 ad a1 7e a4 4b 51 ed 74 b7 2d 00 7e 18 23 7b 88 08 bd 5e b4 4b 12 a6 2a 0d 19 d1 4e fb 33 19 12 09 12 7a 20 20 25 68 93 c4 af 57 f1 56 32 8e
+
+/* handshake1 */
+
+master_secret:
+5ceccf7342151cfb0e8c86a0a63ebfcbbeb80c48da24c93d1f02b7828e3249b43808a77b71baad2f1a8b93ab0db6f0b1
+key_block:
+  client_MAC_secret:	d34fec533adaab09441e65a9ae13154fc44fcded
+  server_MAC_secret:	bd61d4eeacae81bddcc116968800353dad3edd12
+  client_write_key:	e9118d20ba7cd765995c2033bd8e2cbe
+  server_write_key:	01cdd239693d8b203241b7635d3f09fc
+  client_IV:		b2109802adf4f08c15b22816d1d668fb
+  server_IV:		8507d8b2e51fa89d40000e2b2436b0b1
+cleartext:
+1400000c000300000000000c0963da17108dc8e481275891923704d906485a1d3767250f5e134d135d1e491003030303
+MAC (valid):
+923704d906485a1d3767250f5e134d135d1e4910
+
 #endif
