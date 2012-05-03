436491305306b6180634f3b557f18babe95fd59c
Olaf Bergmann <bergmann@tzi.org>
2012-05-03 15:26:49 +0200

    network message queue

---
+++ netq.c
@@ -0,0 +1,108 @@
+/* netq.h -- Simple packet queue
+ *
+ * Copyright (C) 2010--2012 Olaf Bergmann <bergmann@tzi.org>
+ *
+ * This file is part of the library tinyDTLS. Please see the file
+ * LICENSE for terms of use.
+ */
+
+#include "debug.h"
+#include "netq.h"
+
+#ifdef HAVE_ASSERT_H
+#include <assert.h>
+#else
+#ifndef assert
+#warning "assertions are disabled"
+#  define assert(x)
+#endif
+#endif
+
+#ifndef WITH_CONTIKI
+static inline netq_t *
+netq_malloc_node() {
+  return (netq_t *)malloc(sizeof(netq_t));
+}
+
+static inline void
+netq_free_node(netq_t *node) {
+  free(node);
+}
+
+/* FIXME: implement Contiki's list functions using utlist.h */
+
+#else /* WITH_CONTIKI */
+#include "list.h"
+#include "memb.h"
+
+MEMB(netq_storage, netq_t, NETQ_MAXCNT);
+
+static inline netq_t *
+netq_malloc_node() {
+  return (netq_t *)memb_alloc(&netq_storage);
+}
+
+static inline void
+netq_free_node(netq_t *node) {
+  memb_free(&netq_storage, node);
+}
+#endif /* WITH_CONTIKI */
+
+void
+netq_init() {
+#ifdef WITH_CONTIKI
+  memb_init(&netq_storage);
+#endif /* WITH_CONTIKI */
+}
+
+int 
+netq_insert_node(netq_t **queue, netq_t *node) {
+  netq_t *p;
+
+  assert(queue);
+  assert(node);
+
+  p = (netq_t *)list_head((list_t)queue);
+  while(p && p->t <= node->t)
+    p = list_item_next(p);
+
+  if (p)
+    list_insert((list_t)queue, p, node);
+  else
+    list_push((list_t)queue, node);
+
+  return 1;
+}
+
+
+netq_t *
+netq_node_new() {
+  netq_t *node;
+  node = netq_malloc_node();
+
+#ifndef NDEBUG
+  if (!node)
+    dsrv_log(LOG_WARN, "netq_node_new: malloc\n");
+#endif
+
+  if (node)
+    memset(node, 0, sizeof(netq_t));
+
+  return node;  
+}
+
+void 
+netq_node_free(netq_t *node) {
+  if (node)
+    netq_free_node(node);
+}
+
+void 
+netq_delete_all(netq_t *queue) {
+  netq_t *p;
+  if (queue) {
+    while((p = list_pop(queue)))
+      netq_free_node(p); 
+  }
+}
+
---
+++ netq.h
@@ -0,0 +1,72 @@
+/* netq.h -- Simple packet queue
+ *
+ * Copyright (C) 2010--2012 Olaf Bergmann <bergmann@tzi.org>
+ *
+ * This file is part of the library tinyDTLS. Please see the file
+ * LICENSE for terms of use.
+ */
+
+#ifndef _NETQ_H_
+#define _NETQ_H_
+
+#include "config.h"
+#include "global.h"
+#include "time.h"
+
+/**
+ * \defgroup netq Network Packet Queue
+ * The netq utility functions implement an ordered queue of data packets
+ * to send over the network and can also be used to queue received packets
+ * from the network.
+ * @{
+ */
+
+#ifndef NETQ_MAXCNT
+#define NETQ_MAXCNT 4 /**< maximum number of elements in netq structure */
+#endif
+
+/** 
+ * Datagrams in the netq_t structure have a fixed maximum size of
+ * DTLS_MAX_BUF to simplify memory management on constrained nodes. */ 
+typedef unsigned char netq_packet_t[DTLS_MAX_BUF];
+
+typedef struct netq_t {
+  struct netq_t *next;
+
+  clock_time_t t;	        /**< when to send PDU for the next time */
+  unsigned char retransmit_cnt;	/**< retransmission counter, will be removed when zero */
+  unsigned int timeout;		/**< randomized timeout value */
+
+  session_t remote;		/**< remote address */
+#if 0
+  coap_tid_t id;		/**< unique transaction id */
+#endif
+
+  size_t length;		/**< actual length of data */
+  netq_packet_t data;		/**< the datagram to send */
+} netq_t;
+
+/** 
+ * Adds a node to the given queue, ordered by their time-stamp t.
+ * This function returns @c 0 on error, or non-zero if @p node has
+ * been added successfully.
+ *
+ * @param queue A pointer to the queue head where @p node will be added.
+ * @param node  The new item to add.
+ * @return @c 0 on error, or non-zero if the new item was added.
+ */
+int netq_insert_node(netq_t **queue, netq_t *node);
+
+/** Destroys specified node and releases any memory that was allocated
+ * for the associated datagram. */
+void netq_node_free(netq_t *node);
+
+/** Removes all items from given queue and frees the allocated storage */
+void netq_delete_all(netq_t *queue);
+
+/** Creates a new node suitable for adding to a netq_t queue. */
+netq_t *netq_node_new();
+
+/**@}*/
+
+#endif /* _NETQ_H_ */
