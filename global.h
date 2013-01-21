7aedf641a8f410bf28e77e06e468f8f232608400
Olaf Bergmann <bergmann@tzi.org>
2013-01-21 18:22:16 +0100

    made inclusion sys/socket.h and netinet.h optional

--- global.h
+++ global.h
@@ -38,7 +38,15 @@
 #endif
 
 #include <string.h>
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
+#endif
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
 #ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
 #endif
