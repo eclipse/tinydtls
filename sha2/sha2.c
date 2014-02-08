26a356381ae0b212e1c91ad159167252d1305385
Hauke Mehrtens <hauke@hauke-m.de>
2014-02-08 15:22:31 +0100

    sha2: use size of struct and not of pointer in memset()
    The size of the struct should be given here and not the size of the
    pointer. clang pointed out this error.
    
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

--- sha2/sha2.c
+++ sha2/sha2.c
@@ -657,7 +657,7 @@ void SHA256_Final(sha2_byte digest[], SHA256_CTX* context) {
 	}
 
 	/* Clean up state data: */
-	MEMSET_BZERO(context, sizeof(context));
+	MEMSET_BZERO(context, sizeof(*context));
 	usedspace = 0;
 }
 
@@ -678,7 +678,7 @@ char *SHA256_End(SHA256_CTX* context, char buffer[]) {
 		}
 		*buffer = (char)0;
 	} else {
-		MEMSET_BZERO(context, sizeof(context));
+		MEMSET_BZERO(context, sizeof(*context));
 	}
 	MEMSET_BZERO(digest, SHA256_DIGEST_LENGTH);
 	return buffer;
