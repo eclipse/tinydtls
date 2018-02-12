0583dad95250c1d985305cc41d907f705e9836c2
Olaf Bergmann <bergmann@tzi.org>
2018-02-12 15:25:16 +0100

    dtls.c: removed double definition caused by conflicting merge
    Change-Id: Ib676d0ce1579e6262ecdf78a0bac81de1fc5acdc

--- dtls.c
+++ dtls.c
@@ -64,11 +64,6 @@
 MEMARRAY(dtlscontext_storage, sizeof(dtls_context_t), DTLS_CONTEXT_MAX)
 #endif /* RIOT_VERSION */
 
-#ifdef RIOT_VERSION
-#include "memarray.h"
-MEMARRAY(dtlscontext_storage, sizeof(dtls_context_t), DTLS_CONTEXT_MAX)
-#endif
-
 #define dtls_set_version(H,V) dtls_int_to_uint16((H)->version, (V))
 #define dtls_set_content_type(H,V) ((H)->content_type = (V) & 0xff)
 #define dtls_set_length(H,V)  ((H)->length = (V))
