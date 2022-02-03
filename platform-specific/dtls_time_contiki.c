b1ebfc63212c9199c9085fe4d594dc4f694a5f14
Olaf Bergmann <bergmann@tzi.org>
2022-02-03 08:37:37 +0100

    dtls_time: Moved platform-specific code to separate files
    Change-Id: I79d6b07be77f496f592fe53833c34666dd068d1a

--- platform-specific/dtls_time_contiki.c
+++ platform-specific/dtls_time_contiki.c
@@ -17,31 +17,15 @@
 #include "tinydtls.h"
 #include "dtls_time.h"
 
-time_t dtls_clock_offset;
+clock_time_t dtls_clock_offset;
 
 void
 dtls_clock_init(void) {
-#ifdef HAVE_TIME_H
-  dtls_clock_offset = time(NULL);
-#else
-#  ifdef __GNUC__
-  /* Issue a warning when using gcc. Other prepropressors do 
-   *  not seem to have a similar feature. */ 
-#   warning "cannot initialize clock"
-#  endif
-  dtls_clock_offset = 0;
-#endif
+  clock_init();
+  dtls_clock_offset = clock_time();
 }
 
-void dtls_ticks(dtls_tick_t *t) {
-#ifdef HAVE_SYS_TIME_H
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  *t = (tv.tv_sec - dtls_clock_offset) * DTLS_TICKS_PER_SECOND 
-    + (tv.tv_usec * DTLS_TICKS_PER_SECOND / 1000000);
-#else
-#error "clock not implemented"
-#endif
+void
+dtls_ticks(dtls_tick_t *t) {
+  *t = clock_time();
 }
-
-
--- platform-specific/dtls_time_riot.c
+++ platform-specific/dtls_time_riot.c
@@ -17,31 +17,14 @@
 #include "tinydtls.h"
 #include "dtls_time.h"
 
-time_t dtls_clock_offset;
+dtls_tick_t dtls_clock_offset;
 
 void
 dtls_clock_init(void) {
-#ifdef HAVE_TIME_H
-  dtls_clock_offset = time(NULL);
-#else
-#  ifdef __GNUC__
-  /* Issue a warning when using gcc. Other prepropressors do 
-   *  not seem to have a similar feature. */ 
-#   warning "cannot initialize clock"
-#  endif
-  dtls_clock_offset = 0;
-#endif
+  dtls_clock_offset = xtimer_now64().ticks64;
 }
 
-void dtls_ticks(dtls_tick_t *t) {
-#ifdef HAVE_SYS_TIME_H
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  *t = (tv.tv_sec - dtls_clock_offset) * DTLS_TICKS_PER_SECOND 
-    + (tv.tv_usec * DTLS_TICKS_PER_SECOND / 1000000);
-#else
-#error "clock not implemented"
-#endif
+void
+dtls_ticks(dtls_tick_t *t) {
+  *t = xtimer_now64().ticks64 -dtls_clock_offset;
 }
-
-
